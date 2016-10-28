265f16c63422cff8ee46050187ba448a1c6937e8
Jpchen2012 <chenjianping@ustc.edu>
2016-10-28 16:21:15 +0800

    Update indexbuilder.cpp

--- indexbuilder.cpp
+++ indexbuilder.cpp
@@ -1,5 +1,12 @@
+/* Qalinger an ultrafast short read aligner
+Copyright (C) 2016 BGI-shenzhen
+Jianping Chen <chenjianping@genomics.cn>
+Qaligner is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Qaligner is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
-/* This is a reference index building program for the reference smaller than 4G */
+/*This part of code is a reference index building program for the reference sequence smaller than 4G */
 
 #include <stdio.h>
 #include <stdlib.h>
