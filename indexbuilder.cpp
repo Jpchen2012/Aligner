076d6773a9fd6dd537382100dde5777dd66d6e73
Jpchen2012 <chenjianping@ustc.edu>
2016-11-03 15:35:09 +0800

    Update indexbuilder.cpp

--- indexbuilder.cpp
+++ indexbuilder.cpp
@@ -1,4 +1,5 @@
-/* Qalinger an ultrafast short read aligner
+/* 
+Qalinger an ultrafast short read aligner
 Copyright (C) 2016 BGI-shenzhen
 Jianping Chen <chenjianping@genomics.cn>
 Qaligner is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
